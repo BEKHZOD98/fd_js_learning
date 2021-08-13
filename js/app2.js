6947f7644d8ceceff673585bcd28b1deff4bf144
Bekhzod <bexzod9834@gmail.com>
2021-08-13 10:47:29 +0900

    created new practice app2,js file

---
+++ js/app2.js
@@ -0,0 +1,32 @@
+function calc(num1,num2){
+    if(num1>num2){
+        console.log(num2);
+    }else{
+        console.log(num2);
+    }
+
+}
+calc(2,3);
+
+
+function isLandscape(width,height){
+    if(width>height){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+isLandscape(6,9);
+
+function isLandsScapce(width,height){
+    let result =(width>height && width>= height) ; 
+    console.log(result);
+}
+isLandsScapce(9,4);
+
+
+
+
+
+
+
