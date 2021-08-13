e4403657f41fa2118a2058056a966c15889b55e6
Bekhzod <bexzod9834@gmail.com>
2021-08-13 16:05:31 +0900

    created fizzBuzz_algorithm file

---
+++ js/fizzBuzz_Algorithm.js
@@ -0,0 +1,15 @@
+function fizzBuzz(input) {
+    if (typeof input !== 'number') {
+        return 'This variable is not number !';
+    } if ((input % 3 === 0) && (input % 5 === 0)) {
+        return 'fizzbuzz';
+    }
+    if (input % 3 === 0) {
+        return 'fizz';
+    } if (input % 5 === 0) {
+        return 'buzz';
+    }
+    return input;
+}
+
+console.log(fizzBuzz(15));
