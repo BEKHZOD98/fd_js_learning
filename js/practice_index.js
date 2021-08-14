60f4607e81a6e2158db2a442a02993095d56aa0f
Bekhzod <bexzod9834@gmail.com>
2021-08-15 01:50:43 +0900

    modified practice_index , added showProperties function and created car object.

--- js/practice_index.js
+++ js/practice_index.js
@@ -1,4 +1,4 @@
-function sum(limit) {
+/*function sum(limit) {
     if (typeof limit !=='number') {
         return 'this is not number please typing only numbers!!';
     }
@@ -10,4 +10,18 @@ function sum(limit) {
     }
     return sum;
 }
-console.log(sum(55));
+console.log(sum(55));*/
+///////////show properties
+ let car = {
+     moder: 'Toyota',
+     make: 'Estima',
+     year: 2016,
+     price:5000
+ }
+ function showProperties(){
+    for(const [key, value] of Object.entries(car)){
+        if(typeof value === 'string'){
+        console.log(`${key}: ${value}`);}
+    }
+ }
+ showProperties(car);
