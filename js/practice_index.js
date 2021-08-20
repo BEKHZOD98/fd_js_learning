771f27778132e022c33ca8442a94cabd5ba1cad9
Bekhzod <bexzod9834@gmail.com>
2021-08-20 10:30:35 +0900

    modified practice_index.js

--- js/practice_index.js
+++ js/practice_index.js
@@ -29,26 +29,52 @@ console.log(sum(55));*/
 
 
 
- const marks = [81, 74, 86, 59, 77];
+/*const marks = [81, 74, 86, 59, 77];
 
- console.log(calculateGrade(marks))
+console.log(calculateGrade(marks))
  
  
- function calculateAverage(array){
-    let sum = 0;
-    for(let student_point of array){
-          sum+=student_point;
-          
+function calculateAverage(array){
+   let sum = 0;
+   for(let student_point of array){
+         sum+=student_point;
+         
+   }
+   let avarage = Math.floor(sum/5);
+   
+   return avarage;
+}
+function calculateGrade(marks){
+    let avrg = calculateAverage(marks);
+   if(avrg < 50) return 'F';
+   if(avrg < 60) return 'D';
+   if(avrg < 70) return 'C';
+   if(avrg < 80) return 'B';
+   return 'A';
+ }*/
+console.log(showPrimeNumbers(20));
+
+function showPrimeNumbers(limit) {
+
+    for (let num=2; num <=limit; num++) {
+        let isPrime = true;
+        for(let factor = 2; factor<num; factor++){
+        if (num % factor === 0) {
+            isPrime = false;
+            break;
+        }
+        
     }
-    let avarage = Math.floor(sum/5);
+    if(isPrime) console.log(num);
     
-    return avarage;
- }
- function calculateGrade(marks){
-     let avrg = calculateAverage(marks);
-    if(avrg < 50) return 'F';
-    if(avrg < 60) return 'D';
-    if(avrg < 70) return 'C';
-    if(avrg < 80) return 'B';
-    return 'A';
-  }
+}}
+
+
+
+
+
+
+
+
+
+
