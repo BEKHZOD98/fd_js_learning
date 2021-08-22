709f2aecda15a75f630b54e9da6541657f592435
Bekhzod <bexzod9834@gmail.com>
2021-08-22 15:13:46 +0900

    modifed ojects.js file

---
+++ js/ojects.js
@@ -0,0 +1,48 @@
+let radius = 1;
+let x = 1;
+let y = 1;
+
+///this is  factory function
+function creatCircle(radius){
+    return {
+        radius,
+        x: 2,
+        y: 2,
+       location:{
+           x: 1,
+           y: 1
+       },
+       isVisible: true,
+       draw(){
+           console.log('circle');
+       }
+       
+    };
+    
+}
+
+let circle1 = creatCircle(3,'hello');
+console.log(circle1);
+let circle2 = creatCircle(5);
+console.log(circle2);
+
+
+
+//////////////////this is constructor function:
+function Circle(radius){
+    this.radius = radius;
+    this.num = 1;
+    this.move = function(){
+        console.log('helle circle');
+    }
+    this.draw = function(){
+        console.log('draw the circle');
+    }
+    this.test =function(){console.log('test');}
+}
+
+const circle5 = new Circle(25);
+circle5.color = 'green';
+circle5.num = 5;
+delete circle5.test;
+console.log(circle5);
