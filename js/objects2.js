ca403a6e5704507eb4aedef7d3ad6d981490ceb4
Bekhzod <bexzod9834@gmail.com>
2021-08-26 22:12:13 +0900

    modified objects2.js

---
+++ js/objects2.js
@@ -0,0 +1,45 @@
+/*let circle = {
+    radius: 1,
+    location:{
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function(){
+        console.log('doira');
+    },
+    move(){ //functionnni qisqartirilgan korinishi
+        console.log('move');
+    }
+}
+circle.draw();*/
+
+
+
+
+
+
+let circle = {
+    radius: 1,
+    location : {
+        x: 1,
+        y: 2
+    },
+    isVisible: true
+
+};
+function draw(){};
+function move(){}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/ojects.js
+++ js/ojects.js
@@ -3,25 +3,25 @@ let x = 1;
 let y = 1;
 
 ///this is  factory function
-function creatCircle(radius){
+function creatCircle(radius) {
     return {
         radius,
         x: 2,
         y: 2,
-       location:{
-           x: 1,
-           y: 1
-       },
-       isVisible: true,
-       draw(){
-           console.log('circle');
-       }
-       
+        location: {
+            x: 1,
+            y: 1
+        },
+        isVisible: true,
+        draw() {
+            console.log('circle');
+        }
+
     };
-    
+
 }
 
-let circle1 = creatCircle(3,'hello');
+let circle1 = creatCircle(3, 'hello');
 console.log(circle1);
 let circle2 = creatCircle(5);
 console.log(circle2);
@@ -29,16 +29,16 @@ console.log(circle2);
 
 
 //////////////////this is constructor function:
-function Circle(radius){
+function Circle(radius) {
     this.radius = radius;
     this.num = 1;
-    this.move = function(){
+    this.move = function () {
         console.log('helle circle');
     }
-    this.draw = function(){
+    this.draw = function () {
         console.log('draw the circle');
     }
-    this.test =function(){console.log('test');}
+    this.test = function () { console.log('test'); }
 }
 
 const circle5 = new Circle(25);
