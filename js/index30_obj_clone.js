76a3f8986d012170ab00790974eb929661e58e75
Bekhzod <bexzod9834@gmail.com>
2021-08-28 12:17:07 +0900

    added spread and assign method

--- js/index30_obj_clone.js
+++ js/index30_obj_clone.js
@@ -4,6 +4,9 @@ const figure = {
         console.log('circle');
     }
 };///birinchi usuli
+const person = {
+    name:'Mark',age:18
+}
 const clone = {};
 for (let key in figure){
     clone[key] = figure[key];
@@ -13,3 +16,9 @@ console.log('cloned by for in loop',clone);
 
 const cloneFigure = Object.assign({}, figure);//assign methodini ichida 1chi parametriga obj berishimiz kere {}, 2chi parametriga esa qaysi objectdan clone olin votgan bosa ushani ismi yoziladi
 console.log('cloned by assign method: ',cloneFigure);
+//bundan tashqadi Object.assign() methodida clone qilin votganida source sifatida birqancha object bersa boladi va target sifatida yangi properity ham qoshsa boladi
+const cloneFigure1 = Object.assign({color:'white'},figure,person)
+
+//Objectdan clone olish usulini yangi usuli
+const cloneFigure2 = {...figure}//spread operatiridir ...
+console.log('spread: ',cloneFigure2);
