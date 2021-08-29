b5f59eb761c988b069ae2c3a8ef26986f92b1559
Bekhzod <bexzod9834@gmail.com>
2021-08-29 11:35:43 +0900

    modified index30.js

---
+++ js/index32.js
@@ -0,0 +1,9 @@
+let greeting = '#hello world#';
+console.log(greeting.trim());
+
+/*console.log(greeting.includes('edl'));
+console.log(greeting.startsWith('he'));*/
+/*let greeting3 = greeting.replace('hello','new');
+console.log(greeting3.toUpperCase());
+console.log(greeting.toLocaleLowerCase());
+console.log(greeting.trimRight());*/
---
+++ js/index33.js
@@ -0,0 +1,37 @@
+/*
+This is object literary
+let laptop = {
+    name: 'Lenova',
+    display: 'IPS',
+    storage: 'SSD',
+    cpu:{
+        cpuBrand:'Intel',
+        cpuCore:4
+    },
+    ram:16,
+    powerOn: function(){
+        console.log('Laptop');
+    }
+};
+console.log(laptop);*/
+
+// This function is factory function.
+function lap(name,dis,storage,cBrand,cCore,ram){
+    let laptop = {
+        name: name,
+        display: dis,
+        storageType: storage,
+        cpu:{
+            cpuBrand:cBrand,
+            cpuCore:cCore
+        },
+        laptopRam:ram,
+        powerOn: function(){
+            console.log('Laptop');
+        }
+    };
+    return laptop;
+    
+}
+let Lenova = lap('lenova','IPS','SSD','Intel',4,16);
+console.log(Lenova);
